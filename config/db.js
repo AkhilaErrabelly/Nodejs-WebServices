a6d8c34e2352444743332ee2eac108958e350c98
AkhilaErrabelly <aerrabelly@gmail.com>
2018-06-11 14:00:43 -0400

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
-    url: "mongodb://aerrabelly:momdad143@ds151840.mlab.com:51840/akhilaerrabelly"
+    url: "mongodb://<username:password@ds151840.mlab.com:51840/akhilaerrabelly"
 
 }
